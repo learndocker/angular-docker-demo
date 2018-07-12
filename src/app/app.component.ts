ab0b7b169579e7e4d7b899abbf7c13764688b753
Julian Fahrer <julian@fahrer.net>
2018-07-11 20:38:21 -0700

    Add basic functionality around adding todos to the UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import {TodoDataService} from './todo-data.service';
+import {Todo} from './todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService]
 })
 
 export class AppComponent {
   title = 'Angular/Docker Demo';
+  constructor(private todoDataService: TodoDataService) {
+  }
+
+  newTodo: Todo = new Todo();
+
+  addTodo() {
+    this.todoDataService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  toggleTodoCompletedStatus(todo) {
+    this.todoDataService.toggleTodoCompletedStatus(todo);
+  }
+
+  removeTodo(todo) {
+  }
+
+  get todos() {
+    return this.todoDataService.getAllTodos();
+  }
 }
+
